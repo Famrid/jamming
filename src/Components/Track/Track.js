d988e1ea31514d3fd74cfa9d5f1a09484213833b
Famrid <lemasson.steven@gmail.com>
2022-04-16 15:50:56 +0200

    add component steps 29

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Track.css";
+
+export class Track extends React.Component {
+    renderAction() {
+        let buttonValue;
+        if(isRemoval == true) {
+            buttonValue = "-";
+        } else {
+            buttonValue = "+";
+        }
+        return <button className="Track-action">{buttonValue}</button>;
+    }
+
+    render() {
+        return 
+            (<div class="Track">
+                <div className="Track-information">
+                     <h3>"track name will go here"</h3>
+                     <p>"track artist will go here, track album will go here"</p>
+                </div>
+                {this.renderAction}
+            </div>)
+        
+    }
+}
